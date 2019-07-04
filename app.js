bc591853a56ac90eba3ad9eede4fce07d2537344
SANTIAGO OTTOLINI <santiago.ottolini98@gmail.com>
2019-07-04 10:43:14 -0300

    LPPA: Pet Solitaire v0.1

--- app.js
+++ app.js
@@ -5,8 +5,15 @@ const initialState= [
     [1,1,1,0,1,1,1],
     [1,1,1,1,1,1,1],
     [undefined, undefined,1,1,1, undefined, undefined],
-    [undefined, undefined,1,1,1, undefined, undefined]
+    [undefined, undefined,1,1,1, undefined, undefined],
     
+];
 
-
-] 
+for (let i = 0; i < initialState.length; i++) {
+    console.log('ROW');
+    for (let j = 0; j < initialState[i].length; j++) {
+        console.log(initialState[i][j]);
+        
+    }
+    
+}   
