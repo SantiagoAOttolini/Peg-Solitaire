64deed0f4f8a151c9c527b55fc12a27bf77ebd54
SANTIAGO OTTOLINI <santiago.ottolini98@gmail.com>
2019-07-12 00:09:06 -0300

    LPPA: DynamicBoard in js + HTML adn CSS

--- js/app.js
+++ js/app.js
@@ -1,29 +1,156 @@
-const initialState = [
-  [undefined, undefined, 1, 1, 1, undefined, undefined],
-  [undefined, undefined, 1, 1, 1, undefined, undefined],
-  [1, 1, 1, 1, 1, 1, 1],
-  [1, 1, 1, 0, 1, 1, 1],
-  [1, 1, 1, 1, 1, 1, 1],
-  [undefined, undefined, 1, 1, 1, undefined, undefined],
-  [undefined, undefined, 1, 1, 1, undefined, undefined]
+var board = [
+  [, , { value: 1 }, { value: 1 }, { value: 1 }, , ,],
+  [, , { value: 1 }, { value: 1 }, { value: 1 }, , ,],
+  [
+    { value: 1 },
+    { value: 1 },
+    { value: 1 },
+    { value: 1 },
+    { value: 1 },
+    { value: 1 },
+    { value: 1 }
+  ],
+  [
+    { value: 1 },
+    { value: 1 },
+    { value: 1 },
+    { value: 0 },
+    { value: 1 },
+    { value: 1 },
+    { value: 1 }
+  ],
+  [
+    { value: 1 },
+    { value: 1 },
+    { value: 1 },
+    { value: 1 },
+    { value: 1 },
+    { value: 1 },
+    { value: 1 }
+  ],
+  [, , { value: 1 }, { value: 1 }, { value: 1 }, , ,],
+  [, , { value: 1 }, { value: 1 }, { value: 1 }, , ,]
 ];
 
-let dynamicBoard = "<ul>";
-
-for (let i = 0; i < initialState.length; i++) {
-  // recorremos el array
-  dynamicBoard += "<li>";
-  for (let j = 0; j < initialState[i].length; j++) {
-    dynamicBoard += "<button></button>";
-  }
-  dynamicBoard += "</li>";
-}
-dynamicBoard += "</ul>";
-
-window.onload = function() {
-  const boardElement = document.getElementById("board"); // obtenemos el board
-
-  //Asignamos dynamicBoard
-
-  boardElement.innerHTML = dynamicBoard;
+var selectedBall = { x: undefined, y: undefined };
+var createId = function(rowN, colN) {
+  return "ball-" + rowN + "-" + colN;
 };
+
+var generateCell = function(cell, rowN, colN) {
+  var html = '<button id="' + createId(rowN, colN) + '" class = "';
+  if (cell && cell.value) {
+    html += "ball-place";
+  } else if (cell && cell.value === 0) {
+    html += "ball-place-empty";
+  } else {
+    html += "no-ball";
+  }
+  html += '"></button>';
+  return html;
+};
+var generateRow = function(row, rowN) {
+  var html = '<div class="row">';
+  for (let j = 0; j < row.length; j++) {
+    html += generateCell(row[j], rowN, j);
+  }
+  html += "</div>";
+  return html;
+};
+var generateBoard = function() {
+  var html = '<div class="row">';
+  for (let i = 0; i < board.length; i++) {
+    //create the rows
+    html += generateRow(board[i], i);
+  }
+  html += "</div>";
+  return html;
+};
+var unselectedBall = function() {
+  if (selectedBall.x !== undefined && selectedBall.y !== undefined) {
+    var prevSelectedId = createId(selectedBall.x, selectedBall.y);
+    document.getElementById(prevSelectedId).className = "ball-place";
+    var suggestion = document.getElementsByClassName("suggestion");
+    for (let i = 0; i < suggestion.length; i++) {
+      suggestion[i].className = "ball-place-empty";
+    }
+  }
+};
+var getElement = function(id) {
+  var element = document.getElementById(id);
+  return element || {};
+};
+
+var showSuggestions = function() {
+  var near = {
+    above: getElement(createId(selectedBall.x - 1, selectedBall.y)),
+    left: getElement(createId(selectedBall.x, selectedBall.y - 1)),
+    right: getElement(createId(selectedBall.x - 1, selectedBall.y + 1)),
+    below: getElement(createId(selectedBall.x + 1, selectedBall.y))
+  };
+  var possible = {
+    above: getElement(createId(selectedBall.x - 2, selectedBall.y)),
+    left: getElement(createId(selectedBall.x, selectedBall.y - 2)),
+    right: getElement(createId(selectedBall.x - 2, selectedBall.y + 2)),
+    below: getElement(createId(selectedBall.x + 2, selectedBall.y))
+  };
+  if (
+    near.above.className === "ball-place" &&
+    possible.above.className === "ball-place-empty"
+  ) {
+    possible.above.className = "suggestion";
+  }
+  if (
+    near.left.className === "ball-place" &&
+    possible.left.className === "ball-place-empty"
+  ) {
+    possible.left.className = "suggestion";
+  }
+  if (
+    near.below.className === "ball-place" &&
+    possible.below.className === "ball-place-empty"
+  ) {
+    possible.below.className = "suggestion";
+  }
+  if (
+    near.right.className === "ball-place" &&
+    possible.right.className === "ball-place-empty"
+  ) {
+    possible.right.className = "suggestion";
+  }
+};
+var selectBall = function(evt) {
+  var ball = evt.target;
+  var idParts = ball.id && ball.id.length ? ball.id.split("-") : [];
+  if (idParts.length === 3) {
+    unselectedBall();
+    if (
+      selectedBall.x === parseInt(idParts[1]) &&
+      selectedBall === parseInt(idParts[2])
+    ) {
+      unselectedBall();
+      selectedBall.x = undefined;
+      selectedBall.y = undefined;
+    } else {
+      selectedBall.x = parseInt(idParts[1]);
+      selectedBall.y = parseInt(idParts[2]);
+      ball.className = "selected";
+      showSuggestions();
+    }
+  }
+  console.log(selectBall);
+};
+var addBallsEventHandlers = function(ball) {
+  for (let i = 0; i < ball.length; i++) {
+    ball[i].onclick = selectBall;
+  }
+};
+var init = function() {
+  const boardElement = document.getElementById("board"); // get the board
+  //Asign dynamicBoard
+
+  boardElement.innerHTML = generateBoard();
+  var ball = boardElement.getElementsByClassName("ball-place");
+  addBallsEventHandlers(ball);
+};
+window.onload = init;
