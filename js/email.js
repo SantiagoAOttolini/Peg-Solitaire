f6c28cc7fe74a915fbdd190a04f800763e9ac892
SANTIAGO OTTOLINI <santiago.ottolini98@gmail.com>
2019-07-31 12:15:04 -0300

    LPPA: File name in CSS without style, add queries.css, no blank lines in HTML, show popup when you loose

--- js/email.js
+++ js/email.js
@@ -1,12 +1,12 @@
-var init = function(){
-    var send = document.getElementById("buttonSumbit");
+var init = function() {
+    var send = document.getElementById("button-sumbit");
     send.onclick = SendEmail;
 }
-var SendEmail = function(){
+var SendEmail = function() {
     var name = document.getElementById("name").value;
     var message = document.getElementById("message").value;
-    console.log(name,message);
-    window.open('mailto:santiago.ottolini97@gmail.com?subject='+name+'&body='+message);
+    console.log(name, message);
+    window.open('mailto:santiago.ottolini97@gmail.com?subject=' + name + '&body=' + message);
 }
 
 window.onload = init;
